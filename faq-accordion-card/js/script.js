15464806fafc8ac2c184c2d1ce3600c23c5acece
Nicolas Puegher <nicopuegher@gmail.com>
2021-05-24 21:58:05 +0200

    Add JavaScript

--- faq-accordion-card/js/script.js
+++ faq-accordion-card/js/script.js
@@ -1,1 +1,17 @@
+const items = document.querySelectorAll('.accordion-item');
+const box = document.querySelector('.image-wrapper__box');
 
+items.forEach((item) => {
+  item.addEventListener('click', () => {
+    box.style.left = '-100px';
+    if (item.classList.contains('active')) {
+      item.classList.toggle('active');
+    } else {
+      box.style.left = '-125px';
+      items.forEach((item) => {
+        item.classList.remove('active');
+      });
+      item.classList.add('active');
+    }
+  });
+});
